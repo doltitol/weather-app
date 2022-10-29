871c1965354827a8a99a2b6bc5aa09924f3ae123
Oludolapo Adegoroye <doltil@yahoo.co.uk>
2022-10-29 17:36:38 +0100

    added current weather body

--- src/components/CurrentWeatherContainer/CurrentWeatherContainer.jsx
+++ src/components/CurrentWeatherContainer/CurrentWeatherContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CurrentWeatherBody from '../CurrentWeatherBody/CurrentWeatherBody';
 import CurrentWeatherHeader from '../CurrentWeatherHeader/CurrentWeatherHeader';
 import "./CurrentWeatherContainer.css"
 
@@ -6,6 +7,15 @@ const CurrentWeatherContainer = () => {
     return (
         <div className="card weather">
             <CurrentWeatherHeader day="Thursday" time="11:00" />
+            <CurrentWeatherBody
+                city="Lagos"
+                country="NG"
+                desc="Light rain"
+                temperature={ 30 }
+                pressure={ 1012 }
+                humidity={ 98 }
+                wind={ 2 }
+                icon="10d" />
         </div>
     )
 }
