794bde59f6cabfa4868a893c5a04b6e80812a660
Oludolapo Adegoroye <doltil@yahoo.co.uk>
2022-10-29 17:07:03 +0100

    added current weather container

---
+++ src/components/CurrentWeatherContainer/CurrentWeatherContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CurrentWeatherContainer.css"
+
+const CurrentWeatherContainer = () => {
+    return (
+        <div className="card weather">
+            hello
+        </div>
+    )
+}
+
+export default CurrentWeatherContainer
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <p className="footer-link">
-            <a href="https://github.com/doltitol/react-weather-app" target="_blank" rel='noreferrer'>
+            <a href="https://github.com/doltitol/weather-app" target="_blank" rel='noreferrer'>
                 Open-source code
             </a>{ " " }
             by Oludolapo Adegoroye
