40a0b4a02b4c077f8da28c4bde32d634008e527b
Oludolapo Adegoroye <doltil@yahoo.co.uk>
2022-10-29 17:47:07 +0100

    added humidity, wing and precipitation

--- src/components/CurrentWeatherBody/CurrentWeatherBody.jsx
+++ src/components/CurrentWeatherBody/CurrentWeatherBody.jsx
@@ -12,6 +12,12 @@ const CurrentWeatherBody = (props) => {
                         </h2>
                     </div>
                     <div className="row">
+                        <div className="col-sm-6">
+                            <div className="row">
+                                <div className="col-md-6"></div>
+                                <div className="col-md-6 weather-details d-flex flex-column align-items-start"></div>
+                            </div>
+                        </div>
                         <div className="col-sm-6 d-flex align-items-center justify-content-center">
                             <img
                                 src={ `http://openweathermap.org/img/wn/${ props.icon }@2x.png` }
